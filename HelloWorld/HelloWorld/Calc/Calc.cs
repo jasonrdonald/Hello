8ab5fa3a3829c6e27c54f4b4e6cfd52da2810b00
jasonrdonald <jasonrdonald@yahoo.co.in>
2015-09-24 02:37:26 -0700

    Calculator

---
+++ HelloWorld/HelloWorld/Calc/Calc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.Calculator
+{
+    public class Calc
+    {
+        private double m_balance;// { get; set; }
+        public double Balance { get { return m_balance; } set { m_balance = value; } }
+        public string name { get; set; }
+
+        public void Withdraw(double amount)
+        {
+            if (m_balance >= amount)
+            {
+                m_balance -= amount;
+            }
+            else
+            {
+                throw new ArgumentException(amount.ToString(), "Withdrawal exceeds balance!");
+            }
+        }
+
+        public Calc(string cname, double balance)
+        {
+            this.Balance = balance;
+            this.name = cname;
+        }
+    }
+}

7197886b272920c2b8a8f789131ca4e190faa0ed
jasonrdonald <jasonrdonald@yahoo.co.in>
2015-09-24 03:26:43 -0700

    Changes

--- HelloWorld/HelloWorld.Tests/UnitTest1.cs
+++ HelloWorld/HelloWorld.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld.Calculator;
 
 namespace HelloWorld.Tests
 {
@@ -10,5 +11,32 @@ namespace HelloWorld.Tests
         public void TestMethod1()
         {
         }
+
+        [TestMethod]
+        public void Withdraw_ValidAmount_ChangesBalance()
+        {
+            // arrange
+            double currentBalance = 10.0;
+            double withdrawal = 1.0;
+            double expected = 9.0;
+            var account = new Calc("JohnDoe", currentBalance);
+            // act
+            account.Withdraw(withdrawal);
+            double actual = account.Balance;
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Withdraw_AmountMoreThanBalance_Throws()
+        {
+            // arrange
+            var account = new Calc("John Doe", 10.0);
+            // act
+            account.Withdraw(11.0);
+            // assert is handled by the ExpectedException
+        }
+
     }
 }
